a4bce6692ef22afb411a4ba682520e205d88379d
developermickey <mukesh.quazma@gmail.com>
2024-09-29 12:30:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,134 @@
+// console.log("hello World");
+// //comments
+// console.log("hello World");
+
+// ctrl + / commands
+// Single Line Comment
+
+// shift + alt + A commands
+/*  multiple 
+write a code for game
+coditon 
+1 aesa hona cvhhaiye 
+2 aesa hona cvhhaiye 
+
+
+*/
+
+/* kjghjkgfhdg
+jklhdsfgklhdfsg
+hjdfhgjkdflg
+ndfgldfjngkl */
+
+// variables
+
+// variable ek tarah se block hote hai jaha par hum value yeah data store kar sakte hai
+// keyword  var, let, const
+// var book = abcd;
+// var book = efgh;
+
+// let name = "Mukesh";
+// name = "Samkisha";
+
+// constants
+
+// const PI = 3.14;
+// PI = 6.28;
+
+// DataTypes
+// 2 types of datatypes
+// primitive / non primitive data = refernce
+
+// primitive
+
+// String = Sare Text aavdvfbv / a
+// Number = Sare Number 1 1.2 1.00000
+// Boolean = only 2 value true / false
+// Null = value asign ki gai ho lekin value null a = null
+// Undefined = jike pas koi value na ho  let a;
+// bigInt
+// Symbol
+
+// refernce type
+
+// () Small brackets method
+// {} Curly Bracket Object
+// [] square brackets Array
+
+//String
+
+// let name = "Samiksha"; // string
+// let fullName = "Samiksha Tiwari"; // string
+// let char = "A"; // char
+
+// Number
+
+// let age = 25;
+// let rollNumber = 1211554646;
+// let bankBalnce = 100.1;
+
+// Boolean
+
+// let isEligible = true; // 1
+// isEligible = false; // 0 // ES6
+
+// var isEligible = true;
+// var isEligible = false;  // ES5
+
+// Null ki value asign hogi lekin wo value null hogi
+
+// let myAge = null;
+
+// console.log(myAge);
+
+// Undefined aesa var jise koi bhi value asign nahi ki gai ho only declaer
+
+// let lastName;
+
+// console.log(lastName);
+
+// Operators
+// artimetic op (+, -, *, /, %)
+
+let a = 10;
+let b = 10;
+
+let z = a + b; // add
+let y = a - b; // sub
+let X = a * b; // multiple
+let Y = a / b; // div
+let mod = a % b; // remainder
+
+// comprision op (==, === , != , !==, <, <=, >, >=)
+
+// let my = 10;
+// let ny = "10";
+// console.log(my == ny); // true
+// console.log(my != ny); // false
+// console.log(my === ny); // false
+// console.log(my !== ny); // true
+
+// let ab = 2;
+// let bc = 3;
+
+// console.log(ab >= bc);
+
+// logical op (&& and, || or, ! not)
+
+// And && 2 condition true hona jaruri hai Driving Li
+// 1. 18+ hona jaruri hai
+// 2. Drive karte aana chaiye
+
+// || or 2 condition men se koi bhi ek condition true hona chahiye
+// 1. Vote Dene ke liye apka 18+
+
+// ! not jo bhi value ulta kar kar dena
+// if true to false bna dena
+// if false to true bna dena
+
+// ternary op (? :)
+
+// let abc = 10;
+// let def = 11;
+
+// console.log(abc === def ? "=" : "!=");
