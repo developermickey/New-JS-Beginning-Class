403bfa8230c53847c2c8fd71e563cb5a966dd956
developermickey <mukesh.quazma@gmail.com>
2024-10-08 17:35:34 +0530

    if else and while loop

--- main.js
+++ main.js
@@ -90,14 +90,14 @@ ndfgldfjngkl */
 // Operators
 // artimetic op (+, -, *, /, %)
 
-let a = 10;
-let b = 10;
+// let a = 10;
+// let b = 10;
 
-let z = a + b; // add
-let y = a - b; // sub
-let X = a * b; // multiple
-let Y = a / b; // div
-let mod = a % b; // remainder
+// let z = a + b; // add
+// let y = a - b; // sub
+// let X = a * b; // multiple
+// let Y = a / b; // div
+// let mod = a % b; // remainder
 
 // comprision op (==, === , != , !==, <, <=, >, >=)
 
@@ -115,7 +115,7 @@ let mod = a % b; // remainder
 
 // logical op (&& and, || or, ! not)
 
-// And && 2 condition true hona jaruri hai Driving Li
+// And && dono condition true hona jaruri hai Driving Li
 // 1. 18+ hona jaruri hai
 // 2. Drive karte aana chaiye
 
@@ -139,11 +139,96 @@ let mod = a % b; // remainder
 // ToDo List
 // Function
 
-
-
 // Today Topic Done
 // basic syntex
 // comments
 // variable
 // datatypes
 // All operators
+
+// if else else if
+
+// let marks = 20;
+
+// // marks >= 80 &&  marks < 100  = Grade A
+// // marks = 60 && marks < 80  = Grade B
+// // marks = 40 && marks < 60  = Grade C
+// // marks = 33 && marks < 40  = Grade D
+
+// // if (marks >= 80 && marks < 100) {
+// //   console.log("Grade A");
+// // } else if (marks >= 60 && marks < 80) {
+// //   console.log("Grade B");
+// // } else if (marks >= 40 && marks < 60) {
+// //   console.log("Grade C");
+// // } else if (marks >= 33 && marks < 40) {
+// //   console.log("Grade D");
+// // } else {
+// //   console.log("Fail");
+// // }
+
+// if (marks >= 21) console.log("Pass");
+// else console.log("Fail");
+
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("Hello World");
+//   i++; // i = i + 1
+// }
+
+// Q.1 print 1 to 5 number mean 1, 2,3 ,4 ,5
+
+// let i = 3;
+
+// while (i <= 5) {
+//   // statement
+//   console.log(i);
+//   i++;
+// }
+
+//  Dry Run
+
+// i       i <= 5      print(i)       i++
+// 1       1 <= 5           1         2
+// 2       2 <= 5           2         3
+// 3       3 <= 5           3         4
+// 4       4 <= 5           4         5
+// 5       5 <= 5           5         6
+// 6       6<=5  stop
+
+// Q.2 print 1 to 10 number mean 1, 2,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// Even Number 2 4 6 8 10
+// Odd Number 1 3 5 7 9 11
+
+// Q print all even numbers from 1 to 20
+
+// 2 4 6 8 10 12 14 16 18 20
+
+// let i = 2;
+
+// while (i < 20) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// Odd Number numbers from 1 to 20
+// 1 3 5 7 9 11 13 15 17 19
+
+// let i = 1;
+// while (i <= 20) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
